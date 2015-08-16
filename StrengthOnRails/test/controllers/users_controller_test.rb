fcbbc0524bc4425d9437a05c4a4fdabb6eeba1f9
Christopher Lee <chmalee@ucsc.edu>
2015-08-16 00:59:57 -0700

    Fixing Edits

--- StrengthOnRails/test/controllers/users_controller_test.rb
+++ StrengthOnRails/test/controllers/users_controller_test.rb
@@ -11,13 +11,13 @@ class UsersControllerTest < ActionController::TestCase
     get :new
     assert_response :success
   end
-=begin
+
   test "should redirect edit when not logged in" do
     get :edit, id: @user
     assert_not flash.empty?
     assert_redirected_to login_url
   end
-=begin
+
   test "should redirect update when not logged in" do
     patch :update, id: @user, user: { name: @user.name, email: @user.email }
     assert_not flash.empty?
